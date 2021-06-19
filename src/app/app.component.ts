f5cfbfce87902a6681d4d53bbd55ed6a0f8f8570
Surasith.k <Surasith.k@kabintech.co.th>
2021-06-20 04:28:26 +0700

    fixed project serve error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'crypto-coinflip-game';
+  balance: number = 0;
+  constructor() {
+  }
+  ngOnInit() {
+    console.log(this.balance);
+  }
 }
