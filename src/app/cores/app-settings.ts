d69c3f55f195b59bd560f3dd74b8ba3f6bcd40d1
Surasith.k <Surasith.k@kabintech.co.th>
2021-06-20 17:24:46 +0700

    remove truffle dir

---
+++ src/app/cores/app-settings.ts
@@ -0,0 +1,31 @@
+import { environment } from "src/environments/environment";
+
+export class AppSettings {
+    // public static envName = "Staging"; // Environment changed, changes this
+    public static envName = environment.name; // Environment changed, changes this
+
+    private static Env = {
+        Development: {
+            envName: "Development",
+            host: "http://localhost:5003",
+            appVersion: "1.0"
+        },
+        Testing: {
+            envName: "Testing",
+            host: "http://localhost/facebot-api",
+            appVersion: "1.0"
+        },
+        Staging: {
+            envName: "Staging",
+            host: "http://103.74.252.183/facebot-api",
+            appVersion: "1.0"
+        },
+        Production: {
+            envName: "Production",
+            host: "http://103.74.254.227/facebot-api",
+            appVersion: "1.0"
+        }
+    };
+
+    public static appBuildNo = "2021061901";
+}
--- truffle/migrations/2_deploy_coinflip.js
+++
@@ -1,5 +0,0 @@
-const KOSCoinflip = artifacts.require("KOSCoinflip");
-
-module.exports = function (deployer, network, accounts) {
-  deployer.deploy(KOSCoinflip, accounts[2], accounts[3]);
-};
